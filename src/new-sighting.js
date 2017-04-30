56fc85158fa226accb55cf5f405032046c22fd84
Renault Young <ryoung@factset.com>
2017-04-30 09:35:54 -0400

    New sighting route change

--- src/new-sighting.js
+++ src/new-sighting.js
@@ -20,7 +20,8 @@ import eagle from "./images/eagle.png"
 import frog from "./images/frog.png"
 import fish from "./images/fish.png"
 import snek from "./images/snek.png"
-import {Button} from "react-bootstrap";
+import { Button } from "react-bootstrap";
+import { Redirect } from 'react-router';
 
 const GettingStartedGoogleMap = withGoogleMap(props => (
   <GoogleMap
@@ -28,12 +29,12 @@ const GettingStartedGoogleMap = withGoogleMap(props => (
     defaultZoom={12}
     defaultCenter={{ lat: 44.4280, lng: -110.5885 }}
     onClick={props.onMapClick}
-  >
+    >
     {props.markers.map(marker => (
       <Marker
         {...marker}
         onRightClick={() => props.onMarkerRightClick(marker)}
-      />
+        />
     ))}
   </GoogleMap>
 ));
@@ -41,11 +42,13 @@ const GettingStartedGoogleMap = withGoogleMap(props => (
 class Map extends Component {
   state = {
     markers: [],
+    name: null,
   };
 
   handleMapLoad = this.handleMapLoad.bind(this);
   handleMapClick = _.noop;
   handleMarkerRightClick = _.noop;
+  handleOnClick = this.handleOnClick.bind(this);
 
   handleMapLoad(map) {
     this._mapComponent = map;
@@ -54,12 +57,20 @@ class Map extends Component {
     }
   }
 
+  handleOnClick(name) {
+    this.setState({ name: name });
+  }
+
   render() {
+    if (this.state.name) {
+      return <Redirect to={'/new-sighting/' + this.state.name} />;
+    }
+
     return (
-      <div style={{height: `100%`}}>
+      <div style={{ height: `100%` }}>
         <Helmet
           title="Animal Watch: New Sighting"
-        />
+          />
         <img src={mapPin} className="new-sightings-pin-image" />
         <GettingStartedGoogleMap
           containerElement={
@@ -72,13 +83,13 @@ class Map extends Component {
           onMapClick={this.handleMapClick}
           markers={this.state.markers}
           onMarkerRightClick={this.handleMarkerRightClick}
-        />
+          />
         <p> Animal Type </p>
-        <Button> <img src={bear} className="animal-type-button" /></Button>
-        <Button> <img src={eagle} className="animal-type-button" /></Button>
-        <Button> <img src={snek} className="animal-type-button" /></Button>
-        <Button> <img src={frog} className="animal-type-button" /></Button>
-        <Button> <img src={fish} className="animal-type-button" /></Button>
+        <Button onClick={() => this.handleOnClick("bear")}><img src={bear} className="animal-type-button" /></Button>
+        <Button onClick={() => this.handleOnClick("eagle")}>><img src={eagle} className="animal-type-button" /></Button>
+        <Button onClick={() => this.handleOnClick("snek")}>><img src={snek} className="animal-type-button" /></Button>
+        <Button onClick={() => this.handleOnClick("frog")}>><img src={frog} className="animal-type-button" /></Button>
+        <Button onClick={() => this.handleOnClick("fish")}>><img src={fish} className="animal-type-button" /></Button>
       </div>
     );
   }
