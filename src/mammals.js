596cc8da22cb85b12db3cda9b9905971487dd24c
Renault Young <ryoung@factset.com>
2017-04-30 12:59:35 -0400

    Create sighting

--- src/mammals.js
+++ src/mammals.js
@@ -21,16 +21,43 @@ import Jackrabbit from './images/jackrabbit.png'
 import RiverOtter from './images/river-otter.png'
 import Beaver from './images/beaver.png'
 
-
-import {Button, ButtonGroup} from "react-bootstrap";
+import { Button, ButtonGroup } from "react-bootstrap";
+import { connect, PromiseState } from 'react-refetch';
+import { browserHistory } from 'react-router';
 
 class Mammals extends Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
+
   handleOnClick(animalName, isDangerZone) {
     this.setState({ mammalName: animalName, isDangerZone: isDangerZone });
+  }
+
+  createSighting(animalName) {
+    this.props.postSighting(
+      {
+        latitude: 44.4180,
+        longitude: -110.595,
+        animal: {
+          name: animalName,
+        },
+      },
+      () => {
+        this.addSuccess();
+        browserHistory.push(`/`);
+      },
+    );
+  }
+
+  addSuccess = this.addSuccess.bind(this);
+
+  addSuccess() {
+    console.log("success");
+    this.refs.successCallback.success(`Sighting created!`, `Hey there!`, {
+      closeButton: true,
+    });
   }
 
   addAlert() {
@@ -40,68 +67,87 @@ class Mammals extends Component {
   }
 
   render() {
-    let mammalName = '', submitButton = '';
-    if (this.state.mammalName) {
-      mammalName = <h2> It's a {this.state.mammalName} </h2>;
-      submitButton = <Button type='submit' bsStyle="success" bsSize='large'> Submit </Button>
-    }
     if (this.state.isDangerZone) {
       this.addAlert();
     }
+
     return (
-        <div>
-          <ToastContainer
-            toastMessageFactory={ToastMessageFactory}
-            ref="container"
-            className="toast-top-right"
+      <div>
+        <ToastContainer
+          toastMessageFactory={ToastMessageFactory}
+          ref="container"
+          className="toast-top-right"
           />
-          <ButtonGroup>
-            <Button><img  src={BrownBear}
-                          alt="Brown Bear"
-                          className="animal-type-button"
-                          onClick={() => this.handleOnClick('Brown Bear', true)} /> </Button>
-            <Button><img  src={Bison}
-                          alt="Bison"
-                          className="animal-type-button"
-                          onClick={() => this.handleOnClick('Bison', true)}/> </Button>
-            <Button><img  src={BighornSheep}
-                          alt="Bighorn Sheep"
-                          className="animal-type-button"
-                          onClick={() => this.handleOnClick('Bighorn Sheep', true)}/> </Button>
-            <Button><img  src={Moose}
-                          alt="Moose"
-                          className="animal-type-button"
-                          onClick={() => this.handleOnClick('Moose', true)}/> </Button>
-            <Button><img  src={Squirrel}
-                          alt="Squirrel"
-                          className="animal-type-button"
-                          onClick={() => this.handleOnClick('Squirrel', false)}/> </Button>
-            <Button><img  src={Wolf}
-                          alt="Grey Wolf"
-                          className="animal-type-button"
-                          onClick={() => this.handleOnClick('Grey Wolf', true)}/> </Button>
-            <Button><img  src={Elk}
-                          alt="Elk"
-                          className="animal-type-button"
-                          onClick={() => this.handleOnClick('Elk', true)}/> </Button>
-            <Button><img  src={Jackrabbit}
-                          alt="Jackrabbit"
-                          className="animal-type-button"
-                          onClick={() => this.handleOnClick('Jackrabbit', false)}/> </Button>
-            <Button><img  src={RiverOtter}
-                          alt="River Otter"
-                          className="animal-type-button"
-                          onClick={() => this.handleOnClick('River Otter', false)}/> </Button>
-            <Button><img  src={Beaver}
-                          alt="Beaver"
-                          className="animal-type-button"
-                          onClick={() => this.handleOnClick('Beaver', false)}/> </Button>
-          </ButtonGroup>
-          {mammalName}
-          {submitButton}
-        </div>
+        <ToastContainer
+          toastMessageFactory={ToastMessageFactory}
+          ref="successCallback"
+          className="toast-top-center"
+          />
+        <ButtonGroup>
+          <Button><img src={BrownBear}
+            alt="Brown Bear"
+            className="animal-type-button"
+            onClick={() => this.handleOnClick('Brown Bear', true)} /> </Button>
+          <Button><img src={Bison}
+            alt="Bison"
+            className="animal-type-button"
+            onClick={() => this.handleOnClick('Bison', true)} /> </Button>
+          <Button><img src={BighornSheep}
+            alt="Bighorn Sheep"
+            className="animal-type-button"
+            onClick={() => this.handleOnClick('Bighorn Sheep', true)} /> </Button>
+          <Button><img src={Moose}
+            alt="Moose"
+            className="animal-type-button"
+            onClick={() => this.handleOnClick('Moose', true)} /> </Button>
+          <Button><img src={Squirrel}
+            alt="Squirrel"
+            className="animal-type-button"
+            onClick={() => this.handleOnClick('Squirrel', false)} /> </Button>
+          <Button><img src={Wolf}
+            alt="Grey Wolf"
+            className="animal-type-button"
+            onClick={() => this.handleOnClick('Grey Wolf', true)} /> </Button>
+          <Button><img src={Elk}
+            alt="Elk"
+            className="animal-type-button"
+            onClick={() => this.handleOnClick('Elk', true)} /> </Button>
+          <Button><img src={Jackrabbit}
+            alt="Jackrabbit"
+            className="animal-type-button"
+            onClick={() => this.handleOnClick('Jackrabbit', false)} /> </Button>
+          <Button><img src={RiverOtter}
+            alt="River Otter"
+            className="animal-type-button"
+            onClick={() => this.handleOnClick('River Otter', false)} /> </Button>
+          <Button><img src={Beaver}
+            alt="Beaver"
+            className="animal-type-button"
+            onClick={() => this.handleOnClick('Beaver', false)} /> </Button>
+        </ButtonGroup>
+        {this.state.mammalName &&
+          <div>
+            <h2>It's a {this.state.mammalName}</h2>
+            <Button
+              type='submit' bsStyle="success" bsSize='large'
+              onClick={() => this.createSighting(this.state.mammalName)}
+              >
+              Submit
+              </Button>
+          </div>
+        }
+      </div>
     );
   }
 }
 
-export default Mammals;
+export default connect(props => ({
+  postSighting: (payload, successCallback) => ({
+    postSightingResponse: {
+      url: `https://animal-watch-api.herokuapp.com/sightings`,
+      method: 'POST',
+      body: JSON.stringify(payload),
+      then: successCallback,
+    },
+  }),
+}))(Mammals);
